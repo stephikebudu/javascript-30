9f587dcaa9e195f84d1761e02a31900ede8bbce2
Stephany Ikebudu <stephikebudu@gmail.com>
2023-01-19 12:07:58 +0800

    completed array project

---
+++ string-transform-task/string-transform.js
@@ -0,0 +1,10 @@
+const string = "hello";
+const stringLength = string.length;
+// function stringTransform(string) {
+    var x;
+    transformedString = x.repeat(stringLength);
+    // return transformedString;
+// };
+
+stringTransform();
+console.log(stringTransform("hello"));
